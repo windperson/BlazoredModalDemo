86a4e94aaf37fa3cd26ea6f8b0e79b92c8347305
Chen-Yu Pao <windperson@gmail.com>
2021-01-19 12:35:40 +0800

    add project code

---
+++ BlazoredModalDemo/Data/ModalResults/The1stModalResult.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazoredModalDemo.Data.ModalResults
+{
+    public class The1stModalResult
+    {
+        public int ModalId { get; set; } = 1;
+        public string Message { get; set; }
+    }
+}
---
+++ BlazoredModalDemo/Data/ModalResults/The2ndModalResult.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazoredModalDemo.Data.ModalResults
+{
+    public class The2ndModalResult
+    {
+        public int ModalId { get; set; } = 2;
+        public string Message { get; set; }
+    }
+}
